0d442e2a381314e9f47ebe356ca63315f6731db7
jalolkhan <jalolkhan888@gmail.com>
2023-01-30 22:48:28 +0500

    easy app

---
+++ shop/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
+
+class Subcategory(models.Model):
+    category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['name']), ]
+        verbose_name = 'subcategory'
+        verbose_name_plural = 'subcategories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_subcategory', args=[self.category.slug, self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
---
+++ shop/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+
+from .models import Category, Product, Subcategory
+# Create your views here.
+
+
+def index(request):
+    categories = Category.objects.all()
+    subcategories = Subcategory.objects.all()
+    products = Product.objects.filter(available=True)
+
+
+    context = {
+        'categories': categories,
+        'subcategories': subcategories,
+        'products': products,
+    }
+    return render(request, 'shop/home/home.html', context)
+
+def product_list(request):
+    categories = Category.objects.all()
+    subcategories = Subcategory.objects.all()
+    products = Product.objects.filter(available=True)
+
+    return render(request,
+                  'shop/product/list.html',
+                  {'categories': categories,
+                   'subcategories': subcategories,
+                   'products': products})
+
+
+# View for product list in site
+def product_list_category(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    subcategory = None
+    subcategories = Subcategory.objects.all()
+    products = Product.objects.filter(available=True)
+    # print(products.filter(category=1))
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'subcategory': subcategory,
+                   'subcategories': subcategories,
+                   'products': products})
+
+def product_list_subcategory(request, category_slug=None, subcategory_slug=None):
+    category = None
+    categories = Category.objects.all()
+    subcategory = None
+    subcategories = Subcategory.objects.all()
+    products = Product.objects.filter(available=True)
+    # print(products.filter(category=1))
+    if category_slug and subcategory_slug:
+        # print(category_slug)
+        # print(category.id)
+        subcategory = get_object_or_404(Subcategory, slug=subcategory_slug)
+        print(subcategory)
+        products = products.filter(subcategory=subcategory)
+        print(products)
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'subcategory': subcategory,
+                   'subcategories': subcategories,
+                   'products': products})
+
+
+# View for single product
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id = id,
+                                slug = slug,
+                                available = True)
+    # Add to cart button
+    return render(request,
+                  'selcorshop/product/detail.html',
+                  {'product': product,})
